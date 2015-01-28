a71a74a3bf6ccd906b80628f014ad6ca4f37ae86
root <eric.yxs@gmail.com>
2015-01-28 16:30:13 +0800

    init

---
+++ newsIt/newsIt/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for newsIt project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'newsIt'
+
+SPIDER_MODULES = ['newsIt.spiders']
+NEWSPIDER_MODULE = 'newsIt.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'newsIt (+http://www.yourdomain.com)'
